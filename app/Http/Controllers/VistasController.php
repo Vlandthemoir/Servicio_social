efe0fe8c10a348cfa5efeb74d1df969063ef4bbc
vlandthemoir <Elthon13_v@outlook.com>
2023-01-30 11:23:23 -0600

    barra de eventos terminada

--- app/Http/Controllers/VistasController.php
+++ app/Http/Controllers/VistasController.php
@@ -9,42 +9,38 @@ use App\Models\Talleres;
 
 class VistasController extends Controller
 {
-  //arreglar esto hacer que los datos se traigan directamente desde el modelo 
-  //aqui declaro las vistas y paso parametros de la base de datos a estas
   //////////////////////////////////////////////////////////////////////////////
   //vistas para el inicio
   public function create_home(){
-    return view('Blog.home');
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
+    return view('Blog.home', compact('eventos'));
   }
   //////////////////////////////////////////////////////////////////////////////
   //vistas para los talleres
   public function create_verano(){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $verano = DB::table('talleres')
                 ->select('nombre','imagen','dias','horario','edades')
                 ->where('tipo', '=', 'Verano')
                 ->get();
-    /*$cantidad = DB::table('talleres')
-                ->select('nombre','imagen','dias','horario','edades')
-                ->where('tipo', '=', 'Verano')
-                ->count();
-    $verano = Talleres::orderBy('id', 'desc')->paginate($cantidad);*/
-    return view('Blog.verano', compact('verano'));
+    return view('Blog.verano', compact('verano','eventos'));
   }
   public function create_permanente(){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $permanente = DB::table('talleres')
                 ->select('nombre','imagen','dias','horario','edades')
                 ->where('tipo', '=', 'Permanente')
                 ->get();
-    /*$cantidad = DB::table('talleres')
-                ->select('nombre','imagen','dias','horario','edades')
-                ->where('tipo', '=', 'Permanente')
-                ->count();
-    $permanente = Talleres::orderBy('id', 'desc')->paginate($cantidad);*/
-    return view('Blog.permanentes', compact('permanente'));
+    return view('Blog.permanentes', compact('permanente','eventos'));
   }
   //////////////////////////////////////////////////////////////////////////////
   //vistas para la galeria
   public function create_galeria(){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $cantidad = DB::table('galeria')->count()/3;
     $medio = round($cantidad);
 
@@ -63,36 +59,44 @@ class VistasController extends Controller
                 ->offset($medio*2)
                 ->limit(intval($medio*2))
                 ->get();
-    return view('Blog.galeria', compact('columna1','columna2','columna3'));
+    return view('Blog.galeria', compact('columna1','columna2','columna3','eventos'));
   }
   public function create_imagen($nombre){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $imagen = DB::table('galeria')
                 ->select('titulo','imagen','fecha','autor','descripcion')
                 ->where('titulo', '=', $nombre)
                 ->get();
-    return view('Blog.imagen' , compact('imagen'));
+    return view('Blog.imagen' , compact('imagen','eventos'));
   }
   //////////////////////////////////////////////////////////////////////////////
   //vistas necesarias para los articulos
   public function create_articulos(){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $articulos = DB::table('articulos')
                 ->select('id','titulo','portada','fecha','categoria','resumen')
                 ->get();
-    return view('Blog.articulos' , compact('articulos'));
+    return view('Blog.articulos' , compact('articulos','eventos'));
   }
   public function create_categoria($categoria){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $articulos = DB::table('articulos')
                 ->select('id','titulo','portada','fecha','categoria','resumen')
                 ->where('categoria', '=', $categoria)
                 ->get();
-    return view('Blog.categoria' , compact('articulos'));
+    return view('Blog.categoria' , compact('articulos','eventos'));
   }
   public function create_articulo($id){
+    $eventos = DB::table('articulos')->select('titulo','fecha')
+              ->where('Categoria','=','Eventos')->get();
     $articulo = DB::table('articulos')
                 ->select('id','titulo','portada','fecha','categoria','cuerpo')
                 ->where('id', '=', $id)
                 ->get();
-    return view('Blog.articulo' , compact('articulo'));
+    return view('Blog.articulo' , compact('articulo','eventos'));
   }
   //////////////////////////////////////////////////////////////////////////////
 
--- resources/views/Layouts/blog.blade.php
+++ resources/views/Layouts/blog.blade.php
@@ -8,30 +8,27 @@
   </head>
   <body>
     <header class="header">
-	    <a  href="{{route('home.blog')}}" class="logo-header"><h1>Centro<span>Cultural</span></h1></a>
+      <a  href="{{route('home.blog')}}" class="logo-header"><h1>Centro<span>Cultural</span></h1></a>
           <div class="events-bar">
             <div class="events-fondo"></div>
             <div class="events-text">
               <p class="events-title">Proximos eventos</p>
-
+              @foreach ($eventos as $evento)
+              <p class="text">{{$evento->titulo}} {{$evento->fecha}}</p>
+              @endforeach
             </div>
           </div>
           <nav class="navbar">
-
             <ul>
               <li><a href="#">Talleres</a>
                 <ul>
-			<li><a href="{{route('verano.blog')}}">Verano</a></li>
-			<li><a href="{{route('permanente.blog')}}">Permamentes</a></li>
+                  <li><a href="{{route('verano.blog')}}">Verano</a></li>
+                  <li><a href="{{route('permanente.blog')}}">Permamentes</a></li>
                 </ul>
               </li>
-	      <li><a href="{{route('articulos.blog')}}">Articulos</a>
-	      </li>
-	      
-	      <li><a href="{{route('galeria.blog')}}">Galeria</a></li>
-
+              <li><a href="{{route('articulos.blog')}}">Articulos</a></li>
+              <li><a href="{{route('galeria.blog')}}">Galeria</a></li>
             </ul>
-
           </nav>
     </header>
     <div class="container">
